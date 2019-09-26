a41f33ec2cb1bbe2d926e341ba4395d4588f996c
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-09-26 14:00:47 +0200

    Add e2e tests on TravisCI

--- test.js
+++ test.js
@@ -4,22 +4,47 @@ const bir = require('./index');
 
 
 const test_nip_entity = {
-    5213621697: 'Ministerstwo Cyfryzacji',
-    6911051353: 'NZOZ (JDG)',
+    5262596640: {
+        city: 'Warszawa',
+        community: 'm. st. Warszawa',
+        county: 'Mokotów',
+        flaat_no: '',
+        house_no: '21',
+        name: 'GŁÓWNY INSPEKTORAT TRANSPORTU DROGOWEGO',
+        nip: '5262596640',
+        postal_code: '02-676',
+        regon: '017427604',
+        regon14: '017427604',
+        regon9: '017427604',
+        street: 'ul. Test-Krucza',
+        teryt: '0918130',
+        voidoeship: 'MAZOWIECKIE',
+    },
+    6911051353: {
+        name: 'Przychodnia Stomatologiczna "VITADENT" Niepubliczny Zakład Opieki Zdrowotnej Jerzy XXXXXXXX',
+        nip: '6911051353',
+        regon14: '390151863',
+        regon9: '390151863',
+        regon: '390151863',
+        postal_code: '59-220',
+        city: 'Legnica',
+        voidoeship: 'DOLNOŚLĄSKIE',
+        community: 'm. Legnica',
+        county: 'M. Legnica',
+        street: 'ul. Test-Wilcza',
+        house_no: '9',
+        flaat_no: '1',
+        teryt: '0954047',
+    },
 };
 
-Object.keys(test_nip_entity).forEach(nip => {
+Object.entries(test_nip_entity).forEach(([nip, expect]) => {
     // BIR reject login twice at the same time.
-    ava.test.serial(`Test NIP handling ${test_nip_entity[nip]} (${nip})`, async t => {
-        t.true(!!process.env.GUS_API_KEY, 'Environment variable GUS_API_KEY with production API-key are required.');
-        const required_fields = ['name', 'nip', 'regon', 'postal_code',
-            'voidoeship', 'community', 'county', 'city', 'street', 'house_no', 'flaat_no'];
-        const client = bir(true);
-        await client.login(process.env.GUS_API_KEY);
+    ava.test.serial(`Test NIP handling ${expect.name} (${nip})`, async t => {
+        const client = bir(process.env.GUS_ENV === 'production');
+        await client.login(process.env.GUS_API_KEY || 'abcde12345abcde12345');
         const entity = await client.search_nip(nip);
         const report = await client.report(entity.regon14, entity.full_report);
-        for (const field of required_fields) {
-            t.true(!!report[field], `Missing field ${field}`);
-        }
+        t.deepEqual(report, expect);
     });
 });
